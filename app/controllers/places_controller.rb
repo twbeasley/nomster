11436f93e297ee97dc3cd3217059ad0fc0e45057
Tristan Beasley <twbeasley13@gmail.com>
2018-06-23 18:34:45 -0400

    27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,47 +1,72 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
+
+
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 5)
+    @places = Place.order(:name).page(params[:page])
+    Place.page(1).limit_value
   end
-  
+
   def new
     @place = Place.new
   end
-  
+
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
   def show
     @place = Place.find(params[:id])
   end
-  
+
   def edit
     @place = Place.find(params[:id])
-    
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
   end
-  
+  end
+
   def update
     @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
-  end
-  
-  def destroy
-    @place = Place.find(params[:id])
+
     if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
-  
-      @place.destroy
+
+  @place.update_attributes(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+  
+
+
+  def destroy
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed, status: :forbidden'
+    end
+    @place.destroy
+    redirect_to root_path
+  end
+
+  private
+
   def place_params
-     params.require(:place).permit(:name, :description, :address)
-  end
-  end
+    params.require(:place).permit(:name, :description, :address)
+  
+
 end
