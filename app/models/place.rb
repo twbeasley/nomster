16eba735246b821f5947abff12d11917d5f1251b
Tristan Beasley <twbeasley13@gmail.com>
2018-06-26 18:30:12 -0400

    31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :name, presence: true, length: {minimum: 4}
   validates :address, presence: true
   validates :description, presence: true
