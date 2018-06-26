4bd765efa65a71f622f149417234bbec0a7f08fb
Tristan Beasley <twbeasley13@gmail.com>
2018-06-26 17:22:15 -0400

    29

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,8 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-
   def index
-    @places = Place.order(:name).page(params[:page])
-    Place.page(1).limit_value
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
@@ -12,61 +10,61 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
     @place = Place.find(params[:id])
+    @comment = Comment.new 
+    @photo = Photo.new
+     
+
   end
 
   def edit
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render text: 'Not Allowed', status: :forbidden
-    end
+  @place = Place.find(params[:id])
 
-    if @place.user != current_user
-      return render text: 'Not Allowed', status: :forbidden
-    end
-
+  if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
   end
-  end
+end
 
   def update
     @place = Place.find(params[:id])
 
     if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
-  end
-
-  @place.update_attributes(place_params)
-  if @place.valid?
-    redirect_to root_path
-  else
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
     render :edit, status: :unprocessable_entity
-  end
-end
-  
-
+    end
+  end 
 
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render text: 'Not Allowed, status: :forbidden'
-    end
+    return render text: 'Not Allowed', status: :forbidden
+  end
     @place.destroy
     redirect_to root_path
   end
+
+
+
+
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
-  
-
-end
+  end
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618180453) do
+ActiveRecord::Schema.define(version: 20180626204008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180618180453) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
